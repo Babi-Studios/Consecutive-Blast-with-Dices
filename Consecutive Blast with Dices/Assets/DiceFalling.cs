242ef6b151dbcd9c316f9a5df27cb86e774a3cab
dontgiveupbabi <66858685+dontgiveupbabi@users.noreply.github.com>
2020-07-03 15:48:07 +0300

    falling of dices added

---
+++ Consecutive Blast with Dices/Assets/DiceFalling.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceFalling : MonoBehaviour
+{
+    Rigidbody myRigidbody = new Rigidbody();
+
+    private float xVelocity;
+    private float yVelocity;
+    private float zVelocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        xVelocity = myRigidbody.velocity.x;
+        yVelocity = myRigidbody.velocity.y;
+        zVelocity = myRigidbody.velocity.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xandzVelocity();
+    }
+
+    private void xandzVelocity()
+    {
+        xVelocity = 0f;
+        zVelocity = 0f;
+        zVelocity = 0f;
+    }
+}
